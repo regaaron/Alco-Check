b71ad8729b7f2945c8f515991a2fcdcb5d846202
aaron lopez <aaron_lopez222@hotmail.com>
2025-10-09 17:30:49 -0600

    Deploy4 Alco-Check

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+   menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 
 }
