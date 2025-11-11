ed7a281779ccd75e3bb2bbcf0ceef630971522ea
aaron lopez <aaron_lopez222@hotmail.com>
2025-11-10 21:56:37 -0600

    update

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,11 +4,12 @@ import { ComoFuncionaComponent } from "../como-funciona/como-funciona.component"
 import { PreguntasFrecuentesComponent } from "../preguntas-frecuentes/preguntas-frecuentes.component";
 import { CorreoComponent } from "../correo/correo.component";
 import { MisionVisionComponent } from "../mision-vision/mision-vision.component";
+import { MapComponent } from "../map/map.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [VideoComponent, ComoFuncionaComponent, PreguntasFrecuentesComponent, CorreoComponent, MisionVisionComponent],
+  imports: [VideoComponent, ComoFuncionaComponent, PreguntasFrecuentesComponent, CorreoComponent, MisionVisionComponent, MapComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
